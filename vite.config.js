8cbaf93389413f1e0b4a849fc93999dab57249f9
Vishek Patel <85006315+vishek-patel@users.noreply.github.com>
2025-04-03 17:58:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ak/', // Update this with your repository name
+  base: '', // Update this with your repository name
   plugins: [react()],
 });
